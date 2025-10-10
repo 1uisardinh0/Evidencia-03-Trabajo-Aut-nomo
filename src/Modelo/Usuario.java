8cff06f1bfc033a7339f35e2c7f2145614d098cb
1uisardinh0 <l.villalobos03@ufromail.cl>
2025-10-09 22:33:24 -0300

    Usuario donde se le implementa una lista correspondiente a cada usuario para su historial de resultado (Asosiación), e agregan los métodos agregarResultado que permite agregar el resultado a la lista correspondiente al usuario y getHistorial el cual devuelve el historial del usuario como una lista inmodificable.

--- src/Modelo/Usuario.java
+++ src/Modelo/Usuario.java
@@ -1,5 +1,9 @@
 package Modelo;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Usuario {
     private String username;
     private String password;
@@ -13,12 +17,25 @@ public class Usuario {
         this.ruleta = new Ruleta(1000);
     }
 
+    //A cada usuario le corresponde un historial de resultado.
+    private final List<Resultado> historial = new ArrayList<>();
+
     public Usuario(){
         this("Invitado", "", "Usuario Invitado");
     }
 
     public boolean validarCredenciales(String u, String p){
         return this.username.equals(u) && this.password.equals(p);
+    }
+
+    //Agrega un resultado al historial del usuario.
+    public void agregarResultado (Resultado r) {
+        this.historial.add(r);
+    }
+
+    //Devuelve el historial como una lista inmodificable.
+    public List<Resultado> getHistorial() {
+        return Collections.unmodifiableList(historial);
     }
 
     //Getters
