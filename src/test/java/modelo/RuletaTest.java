a3892242c04ed7b847df809cc6173b1f3b0dbcce
Luisardinho <l.villalobos03@ufromail.cl>
2025-11-23 19:49:53 -0300

    Intente nuevamente arreglar los problemas relacionados a las pruebas unitarias y seguir una serie de pasos que me otorga Gemini Pro para poder solucionar el problema del pom.xml, la arquitectura de las carpetas del proyecto y diversas opciones más, como forzar la configuración del JDK y compilador, y ejecutar un goal de Maven para compilar y descargar, pese a seguir todos los pasos y estar más de una hora intentando solucionar los problemas, no se logró y sigue mostrando el mismo error "java: package org.junit.jupiter.api does not exist" al intentar ejecutar las pruebas unitarias.

--- src/test/java/modelo/RuletaTest.java
+++ src/test/java/modelo/RuletaTest.java
@@ -2,29 +2,17 @@ package modelo;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class RuletaTest {
 
-    private final Ruleta ruleta = new Ruleta();
+    @Test
+    void girar() {
+    }
+
+    @Test
+    void actualizarSaldo() {
+    }
 
     @Test
     void depositar() {
-        int monto = 1000;
-        int otroMonto = 0;
-
-        boolean deposito = ruleta.depositar(monto);
-        boolean deposito2 = ruleta.depositar(otroMonto);
-
-        assertTrue(deposito);
-        assertTrue(deposito2);
-    }
-
-    @Test
-    void getColor() {
-    }
-
-    @Test
-    void getSaldo() {
     }
 }
